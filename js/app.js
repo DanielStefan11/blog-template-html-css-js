a0c6065390c3c21b6d1fc22daec8363ab6fd05e3
DanielStefan11 <dannielstefan592@gmail.com>
2020-10-07 19:36:27 +0300

    initialized project

---
+++ js/app.js
@@ -0,0 +1,63 @@
+// Scroll to Top Button Start
+let scrollButton = document.getElementById("scroll-top-btn");
+
+scrollButton.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+// Scroll to Top Button End
+
+// Hamburger Menu Start
+const hamburger = document.getElementById("hamburger");
+const navUl = document.getElementById("nav-ul");
+
+hamburger.addEventListener("click", () => {
+  navUl.classList.toggle("show");
+});
+// Hamburger Menu End
+
+// Mobile Search Button Start
+const mobileSearchButton = document.querySelector("#mobile-search-btn");
+const mobileSearchBar = document.querySelector("#mobile-search-bar");
+
+function showSearchBar() {
+  if (mobileSearchBar.style.display === "none") {
+    mobileSearchBar.style.display = "block";
+  } else {
+    mobileSearchBar.style.display = "none";
+  }
+}
+
+mobileSearchButton.addEventListener("click", showSearchBar);
+// Mobile Search Button End
+
+// Category Read More Button Start
+const dots = document.querySelector("#category-info-dots");
+const more = document.querySelector("#category-info-more");
+const readMoreButton = document.querySelector("#category-info-btn");
+
+function moreInfoCategory() {
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    readMoreButton.innerHTML = "Read More";
+    more.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    readMoreButton.innerHTML = "Read Less";
+    more.style.display = "inline";
+  }
+}
+
+readMoreButton.addEventListener("click", moreInfoCategory);
+// Category Read More Button End
